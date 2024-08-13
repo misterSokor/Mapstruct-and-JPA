5f2e4ef4c884569ba2c32b49cb772ee3b3e0c652
mister.Sokor <mister.sokor@gmail.com>
2024-08-13 13:40:05 -0500

    the name of project was changed

--- src/main/java/com/example/mapstructpractice/repository/employee/EmployeeRepository.java
+++ src/main/java/com/example/mapstructpractice/repository/employee/EmployeeRepository.java
@@ -3,8 +3,9 @@ package com.example.mapstructpractice.repository.employee;
 import java.util.List;
 import java.util.Optional;
 import com.example.mapstructpractice.model.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface EmployeeRepository {
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
     Employee save(Employee employee);
     Optional<Employee> findById(Long id);
     Employee findByName(String name);
